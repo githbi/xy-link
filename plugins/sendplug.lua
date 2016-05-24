052d7fff0c0a8c9bce94213cec30dc36d63981c7
mahdimohbi <mahdimohbi35@gmail.com>
2016-05-24 11:36:41 +0430

    Create sendplug.lua

---
+++ plugins/sendplug.lua
@@ -0,0 +1,22 @@
+do
+
+local function run(msg, matches)
+  if matches[1] == "get" or "send" then
+    local file = matches[2]
+    if is_sudo(msg) then
+      local receiver = get_receiver(msg)
+      send_document(receiver, "./plugins/"..file..".lua", ok_cb, false)
+    end
+  end
+end
+
+return {
+  patterns = {
+  "^[/!#](get) (.*)$",
+  "(get) (.*)$",
+  "(send) (.*)$",
+  "[!/#](send) (.*)$"
+  },
+  run = run
+}
+end
